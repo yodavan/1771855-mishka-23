67d921a371476bef763a4d5121118a3a7c9ca98a
yodavan <84133554+yodavan@users.noreply.github.com>
2021-09-12 23:05:58 +0100

    Внес изменения в проект

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 const navMain = document.querySelector(".main-nav");
 const mainNavDropdown = document.querySelector(".main-nav__dropdown");
+const promoButtonLink = document.querySelector(".promo__button-link");
+const modal = document.querySelector(".modal");
+const productButton = document.querySelector(".product__button");
 
 navMain.classList.remove("main-nav--nojs");
 
@@ -10,5 +13,15 @@ mainNavDropdown.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+promoButtonLink.addEventListener('click', function() {
+  if (modal.classList.contains('modal--closed')) {
+    modal.classList.remove('modal--closed');
+    modal.classList.add('modal--opened');
+  } else {
+    modal.classList.add('modal--closed');
+    modal.classList.remove('modal--opened');
   }
 });
